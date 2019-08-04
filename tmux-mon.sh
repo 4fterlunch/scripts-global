29ab2ecbee6d415c9ed9f0357d370c2796852c6f
michael <mholmes2@student.unimelb.edu.au>
2019-08-05 08:57:41 +1000

    update monitor

--- tmux-mon.sh
+++ tmux-mon.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+# tmux session for monitoring system
+
+session="monitor"
+
+# start new session
+tmux new-session -d -s $session -n 'monitor'
+
+# select pane
+tmux selectp -t 1
+
+# send wavemon
+tmux send-keys "wavemon" C-m
+
+# split pane horizontally
+tmux splitw -h -p 70
+
+# send htop
+tmux send-keys "htop" C-m
+
+# attach sess
+tmux attach-session -t $session
